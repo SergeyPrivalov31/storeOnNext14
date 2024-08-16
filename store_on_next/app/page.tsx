53f44be2bf6280b7e7e606bd87d25437f97a3bc8
Sergei Privalov <privalov.s.a89@gmail.com>
2024-08-16 10:40:50 +0400

    CH-2-1 cashing

--- store_on_next/app/page.tsx
+++ store_on_next/app/page.tsx
@@ -1,15 +1,18 @@
+// export const revalidate = 5; //5 sec trottling if refresh page/ work in prod / npm run start
+// in npm run dev mode, next go refresh every time and give new page every time ( no cashing )
+
+import { cookies } from "next/headers";
+
+// export const dynamic = "force-dynamic"; //need to dynamic refresh, it also worked when you add coockies in function
 export default async function Home() {
-  const data = await fetch("https://jsonplaceholder.typicode.com/todos");
-  const todo = await data.json();
-  // throw new Error("Ohh my God!");
+  cookies();
   return (
     <main className="flex align-center juctify-center gap-2">
-      <h2>NEXT-APP</h2>
-      <br />
-      <p>
-        TODOS:
-        {todo[1].title}
-      </p>
+      <div>{Date.now()}</div>
     </main>
   );
 }
+
+//SSG (Static Site Generation) --> by default
+//ISR (Incremental Static Generation)
+//SSR -->  when coockies added
