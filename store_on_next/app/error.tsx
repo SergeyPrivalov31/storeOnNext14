a1b7ab4d0aebd0a24d002d6c957865172a013be9
Sergei Privalov <privalov.s.a89@gmail.com>
2024-08-20 18:39:28 +0400

    CH-2-4 Server Actions

--- store_on_next/app/error.tsx
+++ store_on_next/app/error.tsx
@@ -1,4 +1,14 @@
 "use client";
-export default function Error({ message }: Error) {
-  return <h1>Somthing wrong. {message}</h1>;
+
+import { useEffect } from "react";
+
+export default function Error({ error }: Record<string, any>) {
+  useEffect(() => {
+    // console.log(error);
+  }, [error]);
+  return (
+    <div className="w-full min-h-56 text-lg text-red-500 flex items-center justify-center flex-col">
+      <h2>Somthing wrong. {error.message}</h2>
+    </div>
+  );
 }
--- store_on_next/app/page.tsx
+++ store_on_next/app/page.tsx
@@ -1,16 +1,27 @@
 // export const revalidate = 5; //5 sec trottling if refresh page/ work in prod / npm run start
 // in npm run dev mode, next go refresh every time and give new page every time ( no cashing )
 
+import getPosts from "@/server/actions/get-posts";
 import { cookies } from "next/headers";
 
 // export const dynamic = "force-dynamic"; //need to dynamic refresh, it also worked when you add coockies in function
 export default async function Home() {
   cookies();
-  return (
-    <main className="flex align-center juctify-center gap-2">
-      <div>{Date.now()}</div>
-    </main>
-  );
+  const { error, success } = await getPosts();
+
+  if (error) {
+    throw new Error(error);
+  }
+  if (success)
+    return (
+      <main className="flex align-center juctify-center gap-2">
+        {success?.map((el) => (
+          <div key={el.id}>
+            <h2>{el.title}</h2>
+          </div>
+        ))}
+      </main>
+    );
 }
 
 //SSG (Static Site Generation) --> by default
