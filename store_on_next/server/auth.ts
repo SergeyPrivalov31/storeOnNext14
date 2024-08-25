8a0b4b621981c8d405d8e45c11a7e7e762ea4fff
Sergei Privalov <privalov.s.a89@gmail.com>
2024-08-25 12:48:20 +0400

    CH-5-234 NextAuthV5 GoogleGithubProviders SettingUp / save users with drizzle provider db

---
+++ store_on_next/server/auth.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import { db } from "@/server";
+import Google from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import Github from "next-auth/providers/github";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  session: { strategy: "jwt" },
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+});
